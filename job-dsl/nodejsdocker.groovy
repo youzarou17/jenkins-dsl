e79b891ef2789adea273c6683bd83fa26b6933cc
youzarou17 <121348591+youzarou17@users.noreply.github.com>
2024-08-18 17:20:25 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('youzarou/nodejs')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('jenkins-sonar-docker')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
